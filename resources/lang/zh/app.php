3ba5f54b196fd9aa98ff5eb1353c2bf573cc1bd3
huhongjun <huhongjun@gmail.com>
2020-02-02 14:18:22 +0800

    locale(zh) Chinese

---
+++ resources/lang/zh/app.php
@@ -0,0 +1,123 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | App Language Lines
+    |--------------------------------------------------------------------------
+    |
+    */
+
+    'settings.system' => '系统',
+    'settings.appearance' => '外观',
+    'settings.miscellaneous' => '其它',
+    'settings.support' => '支持',
+    'settings.donate' => '捐助',
+
+    'settings.version' => '版本',
+    'settings.background_image' => '背景图片',
+    'settings.window_target' => '如何打开链接',
+    'settings.window_target.current' => '在这个标签页打开',
+    'settings.window_target.one' => '在同个标签页打开',
+    'settings.window_target.new' => '在新建标签页打开',
+    'settings.homepage_search' => '主页搜索',
+    'settings.search_provider' => '默认搜索提供方',
+    'settings.language' => '语言',
+    'settings.reset' => '恢复默认值',
+    'settings.remove' => '删除',
+    'settings.search' => '搜索',
+    'settings.no_items' => '没有项目',
+
+
+    'settings.label' => '标签',
+    'settings.value' => '值',
+    'settings.edit' => '编辑',
+    'settings.view' => '显示',
+
+    'options.none' => '- 未设置 -',
+    'options.google' => 'Google',
+    'options.ddg' => 'DuckDuckGo',
+    'options.bing' => 'Bing',
+    'options.qwant' => 'Qwant',
+    'options.startpage' => 'StartPage',
+    'options.yes' => '是',
+    'options.no' => '否',
+    'options.nzbhydra' => 'NZBHydra',
+    'options.jackett' => 'Jackett',
+
+    'buttons.save' => '保存',
+    'buttons.cancel' => '退出',
+    'buttons.add' => '添加',
+    'buttons.upload' => '上传图标',
+    'buttons.downloadapps' => '更新应用列表',
+
+    'dash.pin_item' => '固定项目至面板',
+    'dash.no_apps' => '当前没有固定应用, :link1 或 :link2',
+    'dash.link1' => '在此处添加应用',
+    'dash.link2' => '固定已有项目到面板',
+    'dash.pinned_items' => '已固定项目',
+    'dash.switch_user' => '切换用户',
+
+    'apps.app_list' => '应用列表',
+    'apps.view_trash' => '查看垃圾箱',
+    'apps.add_application' => '添加应用',
+    'apps.application_name' => '应用名称',
+    'apps.colour' => '颜色',
+    'apps.icon' => '图标',
+    'apps.pinned' => '固定',
+    'apps.title' => '标题',
+    'apps.hex' => 'Hex colour',
+    'apps.username' => '用户名称',
+    'apps.password' => '密码',
+    'apps.config' => '配置',
+    'apps.apikey' => 'API钥匙',
+    'apps.enable' => '启用',
+    'apps.tag_list' => '标签列表',
+    'apps.add_tag' => '添加标签',
+    'apps.tag_name' => '标签名称',
+    'apps.tags' => '标签',
+    'apps.override' => 'If different to main url',
+    'apps.preview' => '预览',
+    'apps.apptype' => '应用类型',
+    'apps.autologin_url' => '自动登录地址',
+
+    'dashboard' => '主面板',
+
+    'user.user_list' => '用户',
+    'user.add_user' => '添加用户',
+    'user.username' => '用户名',
+    'user.avatar' => '头像',
+    'user.email' => '邮箱',
+    'user.password_confirm' => '确认密码',
+    'user.secure_front' => '允许公开访问前台 - 除非设置了用户密码.',
+    'user.autologin' => 'Allow logging in from a specific URL. Anyone with the link can login.',
+    'user.settings.edit' => '编辑用户信息',
+
+    'url' => 'URL',
+    'title' => '标题',
+    'delete' => '删除',
+    'optional' => '可选',
+    'restore' => '恢复',
+
+    'alert.success.item_created' => '已成功创建项目',
+    'alert.success.item_updated' => '已成功更新项目',
+    'alert.success.item_deleted' => '已成功删除项目',
+    'alert.success.item_restored' => '已成功恢复项目',
+    'alert.success.updating' => '正在更新应用列表...',
+
+    'alert.success.tag_created' => '已成功创建标签',
+    'alert.success.tag_updated' => '已成功修改标签',
+    'alert.success.tag_deleted' => '已成功删除标签',
+    'alert.success.tag_restored' => '已成功恢复标签',
+
+    'alert.success.setting_updated' => '你已经成功编辑该设置',
+    'alert.error.not_exist' => '该设置不存在.',
+
+    'alert.success.user_created' => '已成功创建用户',
+    'alert.success.user_updated' => '已成功修改用户',
+    'alert.success.user_deleted' => '已成功删除用户',
+    'alert.success.user_restored' => '已成功恢复用户',
+
+
+];
